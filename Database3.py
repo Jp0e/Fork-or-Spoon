ae5631c7e586a307d5c080e62b4a7b55543c1be7
Jp0e <jooshpoe@gmail.com>
2025-02-04 21:18:57 -0800

    Revert "Integrating gui to other code"

---
+++ Database3.py
@@ -0,0 +1,54 @@
+# Written by Joshua Fernandez
+# Database for Scrumdogs
+# Created 1/27/2025
+
+
+import csv
+
+
+# The class for our database. 
+class Database:
+    def __init__(self,file = None)->None:
+        # self.file is the only required attribute for this class.
+        # the file attribute has to be a ".csv" file.
+        self.file = file
+
+
+    # This method allows us to input any list of dictionaries into our Scrumabase. 
+    def excel(self, studentlist)->None:
+        """ This method will take a list of dictionary items and
+        write each dictionary item into a row in a CSV File.
+        
+        Takes 1 argument. Requires a list of dictionary items.
+        """
+
+        file = self.file
+
+        # This bit of code opens the file math in the mode we need as well as the character style encoding.
+        with open(file, mode= 'w', newline = '', encoding = 'utf-8') as file:
+            # writer is a variable for the csv.DictWriter function that creates a csv file and takes an argument for th file and the fieldnames.
+            writer = csv.DictWriter(file, fieldnames= studentlist[0].keys())
+            # This is a method from the csv library DictWriter Class.
+            writer.writeheader()
+            # Method to write multiple rows of dictionary items.
+            writer.writerows(studentlist)
+
+
+    # This bit of code allows to to extract the information from the csv into a list of dictionaries.
+    def csv_to_dict(self)-> None:
+        """
+        This function takes each row in a CSV file and makes dictionaries.
+
+        Does not need to take in any arguments. 
+        """
+        file = self.file
+
+        with open(file, mode='r', encoding='utf-8') as file:
+            # The "DictReader" class automatically uses the Headers of the CSV files as keys.
+            csv_reader = csv.DictReader(file)
+            data = [dict(row) for row in csv_reader]  # Convert each row into a dictionary
+        
+        return print(data)
+
+
+
